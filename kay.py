460cfe037b8382f53ea6a930e06b60dc0a35f337
Kaybhee <abdulkabirbadru@gmail.com>
2025-06-02 22:31:21 +0100

    initializing

--- kay.py
+++
@@ -1,6 +0,0 @@
-import pandas pd
-import matplotlib.pyplot as plt
-
-import math
-import numpy as np 
-np.sqrt(5)
--- script.js
+++ script.js
@@ -8,10 +8,32 @@ const main = document.getElementById('section')
 const search = document.getElementById('mSearch');
 const form = document.getElementById('form');
 
+//  function returning the movies
+returnMovies(API_LINK);
+
+// function to handle the movies functionality
 const returnMovies = (url) => {
     fetch(url)
     .then(res => res.json())
     .then((data) => {
-        
+        data.results.forEach(elements => {
+            const cardDiv = document.createElement('div');
+            const rowDiv = document.createElement('div');
+            const columnDiv = document.createElement('div');
+            const img = document.createElement('img')
+            const title = document.createElement('h3');
+            const center = document.createElement('center')
+
+            title.innerHTML = `${elements.title}`;
+            img.src = `${IMG_PATH} + ${elements.poster_path}`;
+            // appending
+            center.appendChild(img);
+            cardDiv.appendChild(center);
+            cardDiv.appendChild(title);
+            columnDiv.appendChild('cardDiv');
+            rowDiv.appendChild('columnDiv');
+
+
+        })
     })
 }
