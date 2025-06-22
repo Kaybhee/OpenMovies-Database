0b778c5cedacc41d13532916026271644d548336
Kaybhee <abdulkabirbadru@gmail.com>
2025-06-22 20:48:44 +0100

    middlewares update

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './server/config.js'
 import reviewRouter from './server/routes/revRoutes.js'
-
+import morgan from 'morgan';
 export const MONGODB_URI = process.env.MONGODB_URI
 
 
-const port = 5000;
+const PORT = 5000;
 // connecting to a database
 connectDB();
-// Initializing Express application
+
+// Middlewares
 const app = express();
 app.use(cors('*'));
 app.use(express.json());
-app.use('*', (req, res) => {
-    res.status(404).json({message: "Not found"})
-})
+app.use(morgan('dev'))
+
 // router requests 
-app.use('api/v1/reviews/', reviewRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 
 
-app.listen(MONGODB_URI || port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT || MONGODB_URI, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 })
 
 
