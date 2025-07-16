60242add6af5bf1605dbfe089274ffc9f2906dde
Kaybhee <abdulkabirbadru@gmail.com>
2025-07-16 23:31:12 +0100

    all

---
+++ const.js
@@ -0,0 +1,5 @@
+export const API_LINK="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ee28a28f616411fbd3b3fbc0bd47fdbe&page=1";
+
+export const SEARCH_LINK="https://api.themoviedb.org/3/search/movie?api_key=ee28a28f616411fbd3b3fbc0bd47fdbe&query=";
+
+export const IMG_PATH='https://image.tmdb.org/t/p/w185'
