f048e50c40a61418d35223c13f19cc1b72123c80
Kaybhee <abdulkabirbadru@gmail.com>
2025-07-17 00:43:50 +0100

    Static files for deployment

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ import connectDB from './server/config.js'
 import reviewRouter from './server/routes/revRoutes.js'
 import morgan from 'morgan';
 import err from './middlewares/error/errCheck.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 export const MONGODB_URI = process.env.MONGODB_URI
@@ -19,6 +26,10 @@ const app = express();
 app.use(cors('*'));
 app.use(express.json());
 app.use(morgan('dev'))
+
+
+// Serve static files html/css/js
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware for errors
 app.use(err)
