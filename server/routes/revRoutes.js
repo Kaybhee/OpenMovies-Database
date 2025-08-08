7ed91ef6501207c702cfd5d920e1aab6b7609723
Kaybhee <abdulkabirbadru@gmail.com>
2025-08-08 11:51:38 +0100

    refactoring codes

--- server/routes/revRoutes.js
+++ server/routes/revRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { getMovieReviews, getReviews, createReview, updateReview, deleteReview } from '../controller/reviewController.js';
+import { getMovieReviews, getReview, createReview, updateReview, deleteReview } from '../controller/reviewController.js';
 
 const reviewRouter = express.Router();
 
 reviewRouter.get('/movies', getMovieReviews);
 reviewRouter.post('/new-review', createReview);
 // review ID
-reviewRouter.get('/:movieId', getReviews);
+reviewRouter.get('/:movieId', getReview);
 reviewRouter.put('/:movieId', updateReview);
 reviewRouter.delete('/:movieId', deleteReview);
 
